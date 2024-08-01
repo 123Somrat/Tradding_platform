bf2ff3d11fcd4e58de3a85ac487444e83a164da5
123Somrat <jafaruddin875@gmail.com>
2024-08-01 10:51:34 +0200

    Create form useing mui

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Form from "./Components/Form";
+
+
+
+function App() {
+ 
+
+  return (
+    <div className="container mx-auto border-2 border-green-300 my-4 ">
+      <h1 className="text-center">Practice mui</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
