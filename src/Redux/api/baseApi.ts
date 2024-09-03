10fbd978e5a8d09c8b659aefdcb71fa9f25d0d98
123Somrat <jafaruddin875@gmail.com>
2024-09-03 21:34:41 +0200

    format code

--- src/Redux/api/baseApi.ts
+++ src/Redux/api/baseApi.ts
@@ -1,27 +1,33 @@
-import {  BaseQueryFn, createApi, FetchArgs, fetchBaseQuery, FetchBaseQueryError } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/query/react";
 
 
 const baseQuery = fetchBaseQuery({
-   baseUrl:'http://localhost:4000/api/v1',
-   credentials:'include',
-   prepareHeaders:()=>{ }
-})
+  baseUrl: "http://localhost:4000/api/v1",
+  credentials: "include",
+  prepareHeaders: () => {},
+});
 
-const customBaseQuery:BaseQueryFn<string | FetchArgs,
-unknown,
-FetchBaseQueryError> = async (args,api,extraOptions)=>{
+const customBaseQuery: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  const response = await baseQuery(args, api, extraOptions);
 
-     const response = await baseQuery(args,api,extraOptions);
-     
-     return response
-}
+  return response;
+};
 
 const baseApi = createApi({
-   reducerPath :"baseApi",
-   baseQuery:customBaseQuery,
-   endpoints: () => ({}),
-   tagTypes:['dueAdded']
-})
-
+  reducerPath: "baseApi",
+  baseQuery: customBaseQuery,
+  endpoints: () => ({}),
+  tagTypes: ["dueAdded"],
+});
 
 export default baseApi;
